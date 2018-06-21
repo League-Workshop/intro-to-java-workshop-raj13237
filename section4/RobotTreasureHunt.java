078a9d0c5ef1cad62eff302e2782e751f0ee20e5
raj13237 <40322786+raj13237@users.noreply.github.com>
2018-06-21 11:59:55 -0700

    Day 4

---
+++ section4/RobotTreasureHunt.java
@@ -0,0 +1,126 @@
+package section4;
+
+import java.awt.KeyEventDispatcher;
+import java.awt.KeyboardFocusManager;
+import java.awt.event.KeyEvent;
+import java.net.MalformedURLException;
+import java.net.URI;
+
+import javax.swing.JOptionPane;
+
+//import org.teachingextensions.logo.ImageBackground;
+//import org.teachingextensions.logo.Paintable;
+//import org.teachingextensions.logo.robot;
+import org.jointheleague.graphical.robot.Robot;
+
+public class RobotTreasureHunt implements KeyEventDispatcher{
+
+	// 1. Create a new mini robot (type "mini" inside the parentheses)
+	Robot r2d2 = new Robot("mini");
+	private void goUp() throws InterruptedException {
+		// 2. Make the robot move up the screen (use setAngle(angle) and microMove(distance))
+			r2d2.setAngle(0);
+			r2d2.microMove(10);
+		
+	}
+
+	private void goDown() throws InterruptedException{
+		// 3. make the robot move down the screen (use setAngle(angle) and microMove(distance))
+		r2d2.setAngle(180);
+		r2d2.microMove(10);
+	}
+
+	private void turnLeft() throws InterruptedException{
+		// 4. Make the robot turn to the left (use setAngle(angle) and microMove(distance))
+		r2d2.setAngle(270);
+		r2d2.microMove(10);
+	}
+
+	private void turnRight() throws InterruptedException{
+		// 5. make the robot turn to the right (use setAngle(angle) and microMove(distance))
+		r2d2.setAngle(90);
+		r2d2.microMove(10);
+	}
+
+	private void spaceBarWasPressed() {
+
+		// 5. Change ROBOTNAME below to match the name of the robot you created in step 1.  THEN, remove the slashes at the beginning of the next two lines
+		int robotXLocation = r2d2.getX();
+		int robotYLocation = r2d2.getY();
+		
+		// 6. Print the robotXLocation and robotYLocation variables to the console 
+		System.out.println(robotXLocation + " " + robotYLocation);
+		// 7. If robot is at same location as the little girl
+		//      --make a pop-up tell the robot where to go next
+		if(robotXLocation == 720 && robotYLocation == 400) {
+			JOptionPane.showMessageDialog(null, "Go to boy");
+		}
+		if(robotXLocation ==200 && robotYLocation == 390) {
+			JOptionPane.showMessageDialog(null, "Go to Ship");
+		}
+		// 8. Give the user subsequent clues at different locations on the image
+		// (pirate robot, swamp, parrots, etc.)
+		
+		// 9.  If the robot is in the final location
+		//     --call the treasureFound() method
+		if(robotXLocation == 480 && robotYLocation == 360) {
+			treasureFound();
+		}
+		
+	}
+
+	private void go() {
+		KeyboardFocusManager.getCurrentKeyboardFocusManager().addKeyEventDispatcher(this);
+		Robot.setWindowImage("section4/treasure_hunt.jpg");
+	
+		JOptionPane.showMessageDialog(null, "Ask the girl for help with your quest. Press the space bar to ask.");
+
+	}
+
+	public boolean dispatchKeyEvent(KeyEvent e) {
+		if (e.getID() == KeyEvent.KEY_PRESSED) {
+			if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+				try {
+					turnRight();
+				} catch (InterruptedException e1) {
+					e1.printStackTrace();
+				}
+			}
+			else if (e.getKeyCode() == KeyEvent.VK_LEFT)
+				try {
+					turnLeft();
+				} catch (InterruptedException e2) {
+					e2.printStackTrace();
+				}
+			else if (e.getKeyCode() == 38)
+				try {
+					goUp();
+				} catch (InterruptedException e1) {
+					e1.printStackTrace();
+				}
+			else if (e.getKeyCode() == KeyEvent.VK_DOWN)
+				try {
+					goDown();
+				} catch (InterruptedException e1) {
+					e1.printStackTrace();
+				}
+			else if (e.getKeyCode() == KeyEvent.VK_SPACE)
+				spaceBarWasPressed();
+		}
+		return false;
+	}
+	
+	static void treasureFound() {
+		try {
+			URI uri = new URI("https://www.youtube.com/watch?v=G0aIg4N6aro");
+			java.awt.Desktop.getDesktop().browse(uri);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+
+	public static void main (String[] args) throws MalformedURLException {
+		new RobotTreasureHunt().go();
+	}
+}
